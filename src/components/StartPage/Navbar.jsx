67e2adf4526288e583adfea6448d6ca1094996b3
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-04-25 16:40:28 +0200

    Boards have been created

--- src/components/StartPage/Navbar.jsx
+++ src/components/StartPage/Navbar.jsx
@@ -1,7 +1,7 @@
 import '../../assets/scss/navbar.scss';
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
